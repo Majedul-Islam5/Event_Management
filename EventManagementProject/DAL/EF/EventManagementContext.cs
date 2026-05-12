5489b18415c502baaebdeeccf22ea6874b59e849
Majedul-Islam5 <mohammadanas241@gmail.com>
2026-05-12 09:46:43 +0600

    all setup

---
+++ EventManagementProject/DAL/EF/EventManagementContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL.EF.Tables;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF;
+
+public partial class EventManagementContext : DbContext
+{
+    public EventManagementContext()
+    {
+    }
+
+    public EventManagementContext(DbContextOptions<EventManagementContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=EventDbContext");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
